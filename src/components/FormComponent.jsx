424ec7954d95d98e2161a2dbc55a69948f37c176
nox <dark.tempus@gmail.com>
2023-11-05 12:56:56 -0500

    desestruturacion del event ,onChange creacion del metodo onInputChange

--- src/components/FormComponent.jsx
+++ src/components/FormComponent.jsx
@@ -1,4 +1,24 @@
+import { useState } from "react"
+
 export const FormComponent = () => {
+
+const[formState,setFormState]=  useState({
+userName:'Nox Opio',
+email: 'dark.tempus@gmail.com',
+password:'999999'
+
+
+})
+
+
+const {userName,email,password }=formState
+
+const onInputChange= ({target})=>{  
+    const {name,value}= target
+    setFormState({...formState})
+}
+
+
     return (
 
         <form>
@@ -8,15 +28,19 @@ export const FormComponent = () => {
                     type="text"
                     className="form-control"
                     name="userName"
-                    placeholder="uerser name" />
+                    placeholder="uerser name"
+                    value={userName} 
+                    onChange={onInputChange}/>
             </div>
 
             <div className="form-group">
-                <label htmlFor="email1">Email address</label>
+                <label htmlFor="email">Email address</label>
                 <input type="email"
                     className="form-control"
-                    name="email1"
+                    name="email"
                     placeholder="Enter email"
+                    value={email}
+                    onChange={onInputChange}
                 />
             </div>
             <div className="form-group">
@@ -25,6 +49,8 @@ export const FormComponent = () => {
                     className="form-control"
                     name="password"
                     placeholder="Password"
+                    value={password}
+                    onChange={onInputChange}
                 />
             </div>
             <div className="form-check">
