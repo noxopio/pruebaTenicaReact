32959912651e6b4975eeba752fd00b627045d164
nox <dark.tempus@gmail.com>
2023-11-05 12:07:38 -0500

    formulario con react

--- src/components/FormComponent.jsx
+++ src/components/FormComponent.jsx
@@ -1,5 +1,42 @@
 export const FormComponent = () => {
-  return (
-    <div>FormComponent</div>
-  )
+    return (
+
+        <form>
+            <div className="form-group">
+                <label for="userName">user name</label>
+                <input
+                    type="text"
+                    className="form-control"
+                    name="userName"
+                    placeholder="uerser name" />
+            </div>
+
+            <div className="form-group">
+                <label for="email1">Email address</label>
+                <input type="email"
+                    className="form-control"
+                    name="email1"
+                    placeholder="Enter email"
+                />
+            </div>
+            <div className="form-group">
+                <label for="password">Password</label>
+                <input type="password"
+                    className="form-control"
+                    name="password"
+                    placeholder="Password"
+                />
+            </div>
+            <div className="form-check">
+            </div>
+            <button type="submit"
+                className="btn btn-primary">
+                Submit
+            </button>
+        </form>
+
+
+
+
+    )
 }
